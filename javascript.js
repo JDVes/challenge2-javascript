5d51d938acfc8caf3e6afaa83627a7189acc9360
Jean-David DOUTI <88549535+JDVes@users.noreply.github.com>
2022-05-10 13:56:48 +0000

    disposition du formulaire

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 //let newp  = document.createElement('p');
- //newP.textcontent= '';
+//newP.textcontent= '';
 
-// function ajouter() {
- //   let  userinput1 =document.getElementById("#saisir");
-  //  let affiche1 = document.getElementById("#tache1");
-  //  affiche1.innerHTML= userinput1.value;
-    
- //}
+function ajout() {
+    let userinput1 = document.querySelector("#saisir");
+    let affiche1 = document.querySelector("#tache1");
+    affiche1.innerHTML = userinput1.value;
 
- let b = document.body;
+}
+
+let b = document.body;
 let p1 = document.getElementById('#p1');
 let p2 = document.getElementById('#p2');
 let newP = document.createElement('#p');
@@ -19,6 +19,3 @@ let eltDel = b.removeChild(p1);
 
 //Supprime p2 du DOM
 p2.remove()
-
-
- 
